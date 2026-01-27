3a4c286f6eb6590d7c1ee154a1936659bbe27140
Caleb <cjgrinton@gmail.com>
2026-01-27 14:39:01 -0400

    create employee landing page, header, and footer

---
+++ ecs/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="w-full bg-white py-2 px-6 border-t border-gray-300">
+            <p className="text-xs text-gray-600">
+                Weyland-Yutani Co. All Rights Reserved
+            </p>
+        </footer>
+    );
+}
---
+++ ecs/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="w-full bg-white py-3 flex justify-center border-b border-gray-300">
+            <img src="/images/weyland.png" />
+        </header>
+    );
+}
