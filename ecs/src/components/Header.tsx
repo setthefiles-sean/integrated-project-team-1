8ef3b96fa4aa3ad748f13ee243d1ef5a2c35de07
Caleb <cjgrinton@gmail.com>
2026-01-28 20:13:13 -0400

    fix header to be a more compact size

--- ecs/src/components/Header.tsx
+++ ecs/src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <header className="w-full bg-white py-3 flex justify-center border-b border-gray-300">
-            <img src="/images/weyland.png" />
+        <header className="w-full bg-white py-1 flex justify-center border-b border-gray-300">
+            <img src="/images/weyland.png" className="h-30 w-auto object-contain" />
         </header>
     );
 }
