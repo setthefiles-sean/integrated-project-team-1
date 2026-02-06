a75db5c1a597ac3850182f642510214454de7f7c
Azzy <marcimay97@gmail.com>
2026-02-06 19:34:21 -0400

    remove unnecessary api route

--- ecs/src/app/api/get/user/route.ts
+++
@@ -1,9 +0,0 @@
-import { getUser } from "@/src/components/Requests";
-import { NextRequest, NextResponse } from "next/server";
-
-// handler for get/user route
-export function POST(request: NextRequest) {
-
-    return getUser();
-    
-}
